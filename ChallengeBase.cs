59575589e2880e14689f210795e1db8e18ccb4d4
Jacob Albano <jacob.albano@gmail.com>
2020-12-01 14:07:17 -0500

    Add project files.

---
+++ ChallengeBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    abstract class ChallengeBase
+    {
+        public int DayNumber { get; }
+
+        public ChallengeBase(int number)
+        {
+            DayNumber = number;
+        }
+
+        public void Run(string input)
+        {
+            RunInner(1, () => Part1(input));
+            RunInner(2, () => Part2(input));
+        }
+
+        private void RunInner(int part, Func<object> action)
+        {
+            try
+            {
+                var result = action();
+                Console.WriteLine($"Part {part} solution result: {result?.ToString() ?? "null"}");
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine($"Part {part} not yet implemented");
+            }
+            catch { throw; }
+        }
+
+        protected void Assert(string message, bool condition)
+        {
+            if (!condition) throw new Exception($"Test failed: {message}");
+            Console.WriteLine($"Test passed: {message}");
+        }
+
+        protected void Assert(string message, Func<bool> conditionRunner)
+        {
+            Assert(message, conditionRunner());
+        }
+
+        protected abstract object Part1(string input);
+        protected abstract object Part2(string input);
+    }
+}
---
+++ Challenges/Day1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Challenges
+{
+    class Day1 : ChallengeBase
+    {
+        public Day1() : base(1)
+        {
+            var testData = @"1721
+979
+366
+299
+675
+1456";
+
+            Assert("Part1 should match example outcome", Part1(testData).Equals(514579));
+            Assert("Part2 should match example outcome", Part2(testData).Equals(241861950));
+        }
+
+        protected override object Part1(string input)
+        {
+            var numbers = input.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .ToArray();
+
+            if (!FindSum2(numbers, 2020, out var a, out var b))
+                throw new Exception("Panic! Unable to solve");
+
+            return a * b;
+        }
+
+        protected override object Part2(string input)
+        {
+            var numbers = input.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .ToArray();
+
+            if (!FindSum3(numbers, 2020, out var a, out var b, out var c))
+                throw new Exception("Panic! Unable to solve");
+
+            return a * b * c;
+        }
+
+        /// <summary>
+        ///  assume input is sorted descending
+        ///  sum the two numbers at either end
+        ///  if sum is larger than target, slice off head and recurse
+        ///  if sum is smaller than target, slice off tail and recurse
+        /// </summary>
+        private bool FindSum2(Span<int> numbers, int target, out int a, out int b)
+        {
+            a = b = -1;
+            if (numbers.IsEmpty)
+                return false;
+
+            a = numbers[0];
+            b = numbers[^1];
+            if (a + b > target)
+                return FindSum2(numbers[1..], target, out a, out b);
+            else if (a + b < target)
+                return FindSum2(numbers[..^1], target, out a, out b);
+
+            return true;
+        }
+
+        /// <summary>
+        ///  assume input is sorted descending
+        ///  find difference between head and target
+        ///  solve for difference with FindSum2
+        ///  if no solution, slice off head and recurse
+        /// </summary>
+        private bool FindSum3(Span<int> numbers, int target, out int a, out int b, out int c)
+        {
+            a = b = c = -1;
+            if (numbers.IsEmpty)
+                return false;
+
+            c = numbers[0];
+            if (FindSum2(numbers[1..], target - c, out a, out b))
+                return true;
+            
+            return FindSum3(numbers[1..], target, out a, out b, out c);
+        }
+    }
+}
